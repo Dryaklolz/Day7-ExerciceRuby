daba3578def919746fe613e410a283f2b41d9f48
Wendel Yoan <wendelyoan@gmail.com>
2020-04-07 16:29:04 +0200

    Ajout de l'exo_13 Wendel Yoan / Jeremy Lhoste

---
+++ exo_13.rb
@@ -0,0 +1,8 @@
+puts "Quelle annee êtes vous venue au monde ?"
+print "> "
+user_birth = Integer(gets.chomp)
+date = 2020
+while user_birth <= 2020
+  puts user_birth
+user_birth = user_birth + 1
+end
