b0a7e030ead974befe6630dc5afd6f67b4a884bb
Wendel Yoan <wendelyoan@gmail.com>
2020-04-07 16:36:42 +0200

    Ajout de l'exo_14.rd Wendel Yoan / Jeremy Lhoste

---
+++ exo_14.rb
@@ -0,0 +1,8 @@
+puts "Ca va peter dans ? (Ajoute des secondes, et regarde la magie operer)"
+print "> "
+user_number = Integer(gets.chomp)
+while user_number >= 0
+  puts user_number
+user_number = user_number - 1
+end
+puts "Et boom ! =P"
