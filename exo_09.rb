82025cdc7201a2c8d57aa68324d03a6ef0e688de
JethroSmylee <jeremy.lhoste@outlook.com>
2020-04-07 12:56:35 +0200

    Ajout exo_09.rb Wendel Yoan / Jeremy Lhoste

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton prénom ?"
+print "> "
+user_prénom = gets.chomp
+puts "et c'est quoi ton nom de famille ?"
+print "> "
+user_nom = gets.chomp
+puts "Bonjour, " + user_prénom +" "+ user_nom + " ! "
