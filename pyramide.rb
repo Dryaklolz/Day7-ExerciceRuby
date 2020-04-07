a10386ca068022e4df3e14e014dfc4d2cb332491
Wendel Yoan <wendelyoan@gmail.com>
2020-04-07 18:43:12 +0200

    Ajout pyramide.rb Wendel Yoan / Jeremy Lhoste

---
+++ pyramide.rb
@@ -0,0 +1,13 @@
+puts "Alors cleopatre, combien d'etages pour votre pyramide ?"
+print "> "
+user_number = Integer(gets.chomp)
+while user_number < 1 || user_number > 25
+	puts "Nous avons seulement trois mois, 25 etages est le maximum"
+	print "> "
+	user_number = Integer(gets.chomp)
+end
+ user_number.times do |i|
+	 i = i + 1
+	 n = user_number - i
+	 puts " "*n + "#"*i
+ end
